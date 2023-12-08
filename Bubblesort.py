2d529ea972c79aaef5516be5ee51609c65efd5a2
aryankeluskar <aryankeluskar6@gmail.com>
2023-12-07 18:24:58 -0700

    bubble, selection in py

---
+++ Bubblesort.py
@@ -0,0 +1,23 @@
+import random
+
+def main():
+   size = int(input("what size: "))
+   a = []
+
+   for i in range(0, size):
+      a.append(int(random.random()*1000+1))
+
+   print("Original Array: "+str(a))
+
+   bubblesort(a)
+
+
+   print("Sorted Array: "+str(a))
+
+def bubblesort(a):
+   for i in range(0, len(a)):
+      for j in range(0, len(a)-1):
+         if(a[j] > a[j+1]):
+            a[j], a[j+1] = a[j+1], a[j]
+
+main()
---
+++ Selectionsort.py
@@ -0,0 +1,23 @@
+import random
+
+def main():
+   size = int(input("what size: "))
+   a = []
+
+   for i in range(0, size):
+      a.append(int(random.random()*1000+1))
+
+   print("Original Array: "+str(a))
+   selectionSort(a)
+   print("Sorted Array: "+str(a))
+
+def selectionSort(a):
+   for i in range(0, len(a)):
+      minIndex = i
+      for j in range(i, len(a)):
+         if(a[j] < a[minIndex]):
+            minIndex = j
+          
+      a[i], a[minIndex] = a[minIndex], a[i]
+
+main()
