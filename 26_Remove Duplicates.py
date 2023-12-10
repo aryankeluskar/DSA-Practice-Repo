f767bedcf629c8d8e9256f243702f8c53f5a2386
aryankeluskar <aryankeluskar6@gmail.com>
2023-12-09 21:51:58 -0700

    leetcode 26,27

---
+++ 26_Remove Duplicates.py
@@ -0,0 +1,12 @@
+# December 9, 2023
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+        return k
---
+++ hashsets.py
@@ -0,0 +1,21 @@
+# December 8, 2023
+
+# Attracted my attention cause fast af
+# interesting to see how it works
+
+hashsets = set()
+hashsets.add(1)
+hashsets.add(2)
+hashsets.add(3)
+
+print(hashsets)
+
+animals = ["cat", "dog", "dog", "rabbit", "horse"]
+
+# get the index of 'dog'
+index = animals.index("dog")
+
+
+print(index)
+
+# Output: 1
