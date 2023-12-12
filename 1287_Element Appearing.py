0c0538d4ceeef44403a85648646ac7ca471980f6
aryankeluskar <aryankeluskar6@gmail.com>
2023-12-11 22:16:13 -0700

    leetcode 1287

---
+++ 1287_Element Appearing.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+   def findSpecialInteger(self, arr: List[int]) -> int:
+      freq_map = defaultdict(int)
+      for i in arr:
+         freq_map[i] += 1
+      
+      for key, value in freq_map.items():
+         if value > (len(arr)/4.0):
+            return key
+      
+      return -1  # If no key has a value greater than as
+         
+         
