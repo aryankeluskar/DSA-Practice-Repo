517f5b1525b725364ec4a2991b43180dfcab0fa9
aryankeluskar <aryankeluskar6@gmail.com>
2024-02-20 11:19:41 -0700

    leetcode 268

---
+++ 268_Missing Number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        tot = (n)*(n+1)/2
+        for i in nums:
+            tot -= i
+        return int(tot)
