04e7cde46bc130264fd6f4183aa54b71d6b7b739
aryankeluskar <aryankeluskar6@gmail.com>
2024-03-19 23:56:47 -0700

    leetcode 36, 1669

---
+++ 1669_Merge In Between Linked Lists.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+        head = list1
+        for _ in range(a-1):
+            list1 = list1.next
+
+        temp = list1.next
+        list1.next = list2
+        while list2.next:
+            list2 = list2.next
+
+        for _ in range(b-a+1):
+            temp = temp.next
+        
+        list2.next = temp
+
+        return head
---
+++ 36_Valid Sudoku.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row=[]
+        col=[]
+        for _ in range(9):
+            row.append([0,0,0,0,0,0,0,0,0])
+            col.append([0,0,0,0,0,0,0,0,0])
+
+        grid = [[[] for _ in range(3)] for _ in range(3)]
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                if board[i][j] in row[i] or board[i][j] in col[j]:
+                    return False
+                if board[i][j] in grid[i//3][j//3]:
+                    return False
+                row[i].append(board[i][j])
+                col[j].append(board[i][j])
+                grid[i//3][j//3].append(board[i][j])
+
+        return True 
