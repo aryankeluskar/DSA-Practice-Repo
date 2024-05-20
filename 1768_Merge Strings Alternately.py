d220495373f065c8594333500bbc948e1d493643
aryankeluskar <aryankeluskar6@gmail.com>
2024-05-20 18:56:54 +0530

    leetcode 1768

---
+++ 1768_Merge Strings Alternately.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        fin = min(len(word1),len(word2))
+        fin = int(fin)
+        s = ""
+
+        for i in range(fin):
+            s+= word1[i]
+            s+= word2[i]
+
+        return s+word1[fin:]+word2[fin:]
