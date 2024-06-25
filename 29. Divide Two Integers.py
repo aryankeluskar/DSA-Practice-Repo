08a43467b4d01d3e7a64fa81064c676a41f927bf
Aryan Keluskar <aryankeluskar6@gmail.com>
2024-06-25 22:31:14 +0530

    leetcode 29

--- 29. Divide Two Integers.py
+++ 29. Divide Two Integers.py
@@ -1,5 +1,15 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
+        """
+        Divide two integers and handle overflow.
+
+        Args:
+            dividend (int): The dividend integer.
+            divisor (int): The divisor integer.
+
+        Returns:
+            int: The result of the division, or the maximum or minimum integer value if there is overflow.
+        """
         if dividend / divisor > 0:
             answer = dividend // divisor
         else:
